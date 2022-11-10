643fe34cb1cb10eb23fbbd36020af4146c67e3d3
Madeline A <madeline.ashley2@gmail.com>
2022-11-10 11:30:17 -0600

    Models generated

---
+++ migrations/20221110172323-create-vinyl.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Vinyls', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      artist: {
+        type: Sequelize.STRING
+      },
+      genre: {
+        type: Sequelize.STRING
+      },
+      published: {
+        type: Sequelize.STRING
+      },
+      rarity: {
+        type: Sequelize.INTEGER
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Vinyls');
+  }
+};
---
+++ models/vinyl.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Vinyl extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Vinyl.init({
+    title: DataTypes.STRING,
+    artist: DataTypes.STRING,
+    genre: DataTypes.STRING,
+    published: DataTypes.STRING,
+    rarity: DataTypes.INTEGER,
+    image: DataTypes.STRING,
+    price: DataTypes.STRING,
+    description: DataTypes.STRING,
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Vinyl',
+  });
+  return Vinyl;
+};
