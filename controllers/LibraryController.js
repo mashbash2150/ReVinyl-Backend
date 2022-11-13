9706cb3228d3dcfd84595354b0a921e74e353b66
Madeline A <madeline.ashley2@gmail.com>
2022-11-13 10:37:58 -0600

    Added Auth Middleware

--- controllers/LibraryController.js
+++ controllers/LibraryController.js
@@ -28,6 +28,8 @@ const GetLibrary = async (req, res) => {
   }
 }
 
+  
+
 const GetLibraryDetails=async(req,res)=>{
   try{
     const library=await Library.findByPk(req.params.library_id) 
@@ -46,11 +48,30 @@ const GetAllLibraries = async (req, res) => {
   }
 }
 
+// const UpdateLibrary = async (req, res) => {
+//   try {
+//     let libraryId = parseInt(req.params.library_id);
+//     // let vinylId = parseInt(req.params.vinyl_id);
+//     // let body = {
+//     //   vinylId,
+//     //   libraryId,
+//     //   ...req.body
+//     // }
+//     let updatedLibrary = await Library.update(req.body, {
+//       where: { id: libraryId },
+//       returning: true
+//     });
+//     res.send(updatedLibrary);
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
 
 module.exports={
   GetLibrary,
   GetAllLibraries,
-  GetLibraryDetails
+  GetLibraryDetails,
+  // UpdateLibrary
 }
 
--- routes/LibraryRouter.js
+++ routes/LibraryRouter.js
@@ -4,8 +4,7 @@ const controller = require('../controllers/LibraryController')
 Router.get('/:user_id', controller.GetLibrary)
 Router.get('/:library_id', controller.GetLibraryDetails)
 Router.get('/', controller.GetAllLibraries)
-// Router.post('/user_id', controller.CreateLibrary)
-// Router.put('/:library_id', controller.UpdateLibrary)
+// Router.put('/:user_id/', controller.UpdateLibrary)
 // Router.delete('/:library_id', controller.DeleteLibrary)
 
 module.exports = Router
