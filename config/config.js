7f14e1753089c5e1d5619af7837dc0aa0cd75b09
Madeline A <madeline.ashley2@gmail.com>
2022-11-14 10:41:19 -0600

    continued conflict troubleshooting

--- config/config.js
+++ config/config.js
@@ -1,23 +1,53 @@
 require('dotenv').config()
-module.exports={
-
+module.exports = {
   development: {
     database: 'revinyl_development',
     dialect: 'postgres'
   },
   test: {
-    database:'revinyl_test',
+    database: 'revinyl_development',
     dialect: 'postgres'
   },
   production: {
-    database: 'revinyl_production',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    dialectOptions:{
-      ssl:{
-        rejectUnauthorized:false,
-        require:true
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+        require: true
       }
     }
-    
   }
 }
+
+
+
+
+
+
+
+
+// require('dotenv').config()
+// module.exports={
+
+//   development: {
+//     database: 'revinyl_development',
+//         // "host": "127.0.0.1",
+//     dialect: 'postgres'
+//   },
+//   test: {
+//     database:'revinyl_test',
+//     dialect: 'postgres'
+//   },
+//   production: {
+//     database: 'revinyl_production',
+//     dialect: 'postgres',
+//     dialectOptions:{
+//       ssl:{
+//         rejectUnauthorized:false,
+//         require:true
+//       }
+//     }
+    
+//   }
+// }
